3a85f0ad9e3e1a79ce609df4abe54f27d155aa4a
The ZZ <64676853+ztimes2@users.noreply.github.com>
2021-08-02 01:24:49 +0800

    Update 1_create_table_spots.up.sql

--- migrations/1_create_table_spots.up.sql
+++ migrations/1_create_table_spots.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE spots (
 	id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL,
-    latitude NUMERIC NOT NULL,
+	latitude NUMERIC NOT NULL,
 	longitude NUMERIC NOT NULL,
 	locality TEXT,
 	country_code TEXT,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+	created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
