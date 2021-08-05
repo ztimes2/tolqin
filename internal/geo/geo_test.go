98b2d758e41935dac46cdcd4b10ef37c2ec09549
The ZZ <ztimes2@pm.me>
2021-08-05 11:47:50 +0000

    Add unit tests for internal/geo

---
+++ internal/geo/geo_test.go
@@ -0,0 +1,64 @@
+package geo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/ztimes2/tolqin/internal/testutil"
+)
+
+func TestCoordinates_Validate(t *testing.T) {
+	tests := []struct{
+		name string
+		coord Coordinates
+		expectedErrFn assert.ErrorAssertionFunc
+	}{
+		{
+			name: "return error for invalid latitude",
+			coord: Coordinates{
+				Latitude: -91,
+				Longitude: 0,
+			},
+			expectedErrFn: testutil.IsValidationError("latitude"),
+		},
+		{
+			name: "return error for invalid latitude",
+			coord: Coordinates{
+				Latitude: 91,
+				Longitude: 0,
+			},
+			expectedErrFn: testutil.IsValidationError("latitude"),
+		},
+		{
+			name: "return error for invalid longitude",
+			coord: Coordinates{
+				Latitude: 0,
+				Longitude: -181,
+			},
+			expectedErrFn: testutil.IsValidationError("longitude"),
+		},
+		{
+			name: "return error for invalid longitude",
+			coord: Coordinates{
+				Latitude: 0,
+				Longitude: 181,
+			},
+			expectedErrFn: testutil.IsValidationError("longitude"),
+		},
+		{
+			name: "return no error",
+			coord: Coordinates{
+				Latitude: 2,
+				Longitude: 70,
+			},
+			expectedErrFn: assert.NoError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T){
+			err := test.coord.Validate()
+			test.expectedErrFn(t, err)
+		})
+	}
+}
