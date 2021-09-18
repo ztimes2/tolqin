1c3719509ac8402d6550e8e0baca900f5ceb9c90
The ZZ <ztimes2@pm.me>
2021-09-18 16:00:39 +0000

    app/api: add godoc to 'internal/pkg/paging' package

--- app/api/internal/pkg/paging/paging.go
+++ app/api/internal/pkg/paging/paging.go
@@ -1,5 +1,7 @@
 package paging
 
+// Limit clamps the given limit value to the given min and max boundaries. If the
+// value is less than min, then the given fallback is used.
 func Limit(limit, min, max, fallback int) int {
 	if limit < min {
 		return fallback
@@ -10,6 +12,7 @@ func Limit(limit, min, max, fallback int) int {
 	return limit
 }
 
+// Offset clamps the given offset value to the given min boundary.
 func Offset(offset, min int) int {
 	if offset < min {
 		return min
