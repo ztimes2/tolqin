f27d746fa6c838d2b40bf508ae96d77b74b08040
The ZZ <ztimes2@pm.me>
2021-09-19 14:16:42 +0000

    app/api: add godoc

--- app/api/internal/pkg/testutil/testutil.go
+++ app/api/internal/pkg/testutil/testutil.go
@@ -5,12 +5,17 @@ import (
 	"github.com/ztimes2/tolqin/app/api/internal/pkg/valerra"
 )
 
+// IsError returns github.com/stretchr/testify package's assert.ErrorAssertionFunc
+// that checks if at least one of the errors in the chain matches target.
 func IsError(target error) assert.ErrorAssertionFunc {
 	return func(t assert.TestingT, err error, i ...interface{}) bool {
 		return assert.Error(t, err) && assert.ErrorIs(t, err, target, i...)
 	}
 }
 
+// AreValidationErrors returns github.com/stretchr/testify package's assert.ErrorAssertionFunc
+// that checks if at least one of the errors in the chain is of type *valerra.Errors
+// and the contains the given targets.
 func AreValidationErrors(targets ...error) assert.ErrorAssertionFunc {
 	return func(t assert.TestingT, err error, i ...interface{}) bool {
 		var vErr *valerra.Errors
