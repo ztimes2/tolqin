f6c0b111dec7f7faef8217b29df8fd2398f6c0b7
The ZZ <ztimes2@pm.me>
2021-09-19 08:05:52 +0000

    app/api: update godoc of 'internal/pkg/valerra'

--- app/api/internal/pkg/valerra/conditions.go
+++ app/api/internal/pkg/valerra/conditions.go
@@ -1,11 +1,15 @@
 package valerra
 
+// StringNotEmpty returns a condition function that checks if the given string is
+// empty or not.
 func StringNotEmpty(s string) func() bool {
 	return func() bool {
 		return s != ""
 	}
 }
 
+// StringLessOrEqual return a condition function that checks if the character length
+// of the given string is less or equal to the given size.
 func StringLessOrEqual(s string, size int) func() bool {
 	return func() bool {
 		return len(s) <= size
