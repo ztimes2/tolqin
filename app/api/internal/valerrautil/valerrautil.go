9e052423862397d8ebc896942493800ef28570ef
The ZZ <ztimes2@pm.me>
2021-09-19 08:09:24 +0000

    app/api: add godoc to 'internal/valerrautil'

--- app/api/internal/valerrautil/valerrautil.go
+++ app/api/internal/valerrautil/valerrautil.go
@@ -2,18 +2,24 @@ package valerrautil
 
 import "github.com/ztimes2/tolqin/app/api/internal/geo"
 
+// IsCountry returns a condition function that checks if the given string is a
+// valid ISO-2 country code.
 func IsCountry(code string) func() bool {
 	return func() bool {
 		return geo.IsCountry(code)
 	}
 }
 
+// IsLatitude returns a condition function that checks if the given number is a
+// valid latitude.
 func IsLatitude(lat float64) func() bool {
 	return func() bool {
 		return geo.IsLatitude(lat)
 	}
 }
 
+// IsLongitude returns a condition function that checks if the given number is a
+// valid longitude.
 func IsLongitude(lon float64) func() bool {
 	return func() bool {
 		return geo.IsLongitude(lon)
