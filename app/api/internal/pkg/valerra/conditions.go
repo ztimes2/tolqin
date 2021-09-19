3cc6c29a81561bfa90e42e9e501863cca51432f8
The ZZ <ztimes2@pm.me>
2021-09-19 09:21:32 +0000

    app/api: refactor validation conditions

--- app/api/internal/pkg/valerra/conditions.go
+++ app/api/internal/pkg/valerra/conditions.go
@@ -1,16 +1,15 @@
 package valerra
 
-// StringNotEmpty returns a condition function that checks if the given string is
-// not empty.
-func StringNotEmpty(s string) func() bool {
+// StringNotEmpty returns a condition that checks if the given string is not empty.
+func StringNotEmpty(s string) Condition {
 	return func() bool {
 		return s != ""
 	}
 }
 
-// StringLessOrEqual returns a condition function that checks if the character length
-// of the given string is less or equal to the given size.
-func StringLessOrEqual(s string, size int) func() bool {
+// StringLessOrEqual returns a condition that checks if the character length of
+// the given string is less or equal to the given size.
+func StringLessOrEqual(s string, size int) Condition {
 	return func() bool {
 		return len(s) <= size
 	}
--- app/api/internal/valerrautil/valerrautil.go
+++ app/api/internal/valerrautil/valerrautil.go
@@ -1,26 +1,29 @@
 package valerrautil
 
-import "github.com/ztimes2/tolqin/app/api/internal/geo"
+import (
+	"github.com/ztimes2/tolqin/app/api/internal/geo"
+	"github.com/ztimes2/tolqin/app/api/internal/pkg/valerra"
+)
 
-// IsCountry returns a condition function that checks if the given string is a
+// IsCountry returns a valerra.Condition that checks if the given string is a
 // valid ISO-2 country code.
-func IsCountry(code string) func() bool {
+func IsCountry(code string) valerra.Condition {
 	return func() bool {
 		return geo.IsCountry(code)
 	}
 }
 
-// IsLatitude returns a condition function that checks if the given number is a
+// IsLatitude returns a valerra.Condition that checks if the given number is a
 // valid latitude.
-func IsLatitude(lat float64) func() bool {
+func IsLatitude(lat float64) valerra.Condition {
 	return func() bool {
 		return geo.IsLatitude(lat)
 	}
 }
 
-// IsLongitude returns a condition function that checks if the given number is a
+// IsLongitude returns a valerra.Condition that checks if the given number is a
 // valid longitude.
-func IsLongitude(lon float64) func() bool {
+func IsLongitude(lon float64) valerra.Condition {
 	return func() bool {
 		return geo.IsLongitude(lon)
 	}
