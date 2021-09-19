b981e038cf739cf023af9dfbc59f768dbdfd31e2
The ZZ <ztimes2@pm.me>
2021-09-19 04:47:19 +0000

    app/api: add godoc to 'internal/pkg/pconv'

--- app/api/internal/pkg/pconv/pconv.go
+++ app/api/internal/pkg/pconv/pconv.go
@@ -1,9 +1,11 @@
 package pconv
 
+// String returns a pointer to the given string.
 func String(s string) *string {
 	return &s
 }
 
+// Float64 returns a pointer to the given float64.
 func Float64(f float64) *float64 {
 	return &f
 }
