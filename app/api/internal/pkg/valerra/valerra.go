9f4b0fd28343bf0f1f9f94858c241bcf676bef08
The ZZ <ztimes2@pm.me>
2021-09-19 08:02:15 +0000

    app/api: add godoc for 'internal/pkg/valerra'

--- app/api/internal/pkg/valerra/valerra.go
+++ app/api/internal/pkg/valerra/valerra.go
@@ -2,16 +2,21 @@ package valerra
 
 import "fmt"
 
+// Validator can be used for validating multiple conditions and catching errors
+// for those that fail.
 type Validator struct {
 	conditions []conditionFn
 }
 
 type conditionFn func() error
 
+// New returns a new *Validator.
 func New() *Validator {
 	return &Validator{}
 }
 
+// IfFalse registers the given condition function to return the given error in
+// case if the condition returns false.
 func (v *Validator) IfFalse(condition func() bool, err error) {
 	v.conditions = append(v.conditions, func() error {
 		if !condition() {
@@ -21,6 +26,9 @@ func (v *Validator) IfFalse(condition func() bool, err error) {
 	})
 }
 
+// Validate validates all the conditions registered to the validator and returns
+// errors for the failed once as *Errors. The function returns nil if all conditions
+// succeed.
 func (v *Validator) Validate() error {
 	var errs []error
 	for _, fn := range v.conditions {
@@ -36,26 +44,33 @@ func (v *Validator) Validate() error {
 	return nil
 }
 
+// IfFalse validates a single condition and immediately returns an error as *Errors
+// in case if the condition returns false. The function returns nil if the condition
+// returns true.
 func IfFalse(condition func() bool, err error) error {
 	v := New()
 	v.IfFalse(condition, err)
 	return v.Validate()
 }
 
+// Errors holds multiple errors.
 type Errors struct {
 	errs []error
 }
 
+// NewErrors wraps the given errors as *Errors.
 func NewErrors(err ...error) *Errors {
 	return &Errors{
 		errs: err,
 	}
 }
 
+// Errors returns underlying errors as slice.
 func (e *Errors) Errors() []error {
 	return e.errs
 }
 
+// Error implements the error interface.
 func (e *Errors) Error() string {
 	if len(e.errs) == 1 {
 		return "1 rule failed validation"
