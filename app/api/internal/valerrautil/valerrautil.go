905e5672b0cffa03649031ae0021a8b6f5a4e6ca
The ZZ <ztimes2@pm.me>
2021-10-20 10:53:06 +0000

    app/api: add auth helpers to 'internal/valerrautil'

--- app/api/internal/valerrautil/valerrautil.go
+++ app/api/internal/valerrautil/valerrautil.go
@@ -1,8 +1,15 @@
 package valerrautil
 
 import (
+	"net/mail"
+
+	"github.com/ztimes2/tolqin/app/api/internal/auth"
 	"github.com/ztimes2/tolqin/app/api/internal/geo"
 	"github.com/ztimes2/tolqin/app/api/internal/pkg/valerra"
+)
+
+const (
+	maxEmailLength = 254
 )
 
 // IsCountry returns a valerra.Condition that checks if the given string is a
@@ -26,5 +33,45 @@ func IsLatitude(lat float64) valerra.Condition {
 func IsLongitude(lon float64) valerra.Condition {
 	return func() bool {
 		return geo.IsLongitude(lon)
+	}
+}
+
+// IsEmail returns a valerra.Condition that checks if the given string is a valid
+// e-mail address.
+func IsEmail(email string) valerra.Condition {
+	return func() bool {
+		if len(email) > maxEmailLength {
+			return false
+		}
+
+		addr, err := mail.ParseAddress(email)
+		if err != nil {
+			return false
+		}
+		if addr.Name != "" {
+			return false
+		}
+		return true
+	}
+}
+
+// IsPassword returns a valerra.Condition that checks if the given string is a valid
+// password.
+func IsPassword(password string) valerra.Condition {
+	return func() bool {
+		return auth.IsPassword(password)
+	}
+}
+
+// IsRoleIn returns a valerra.Condition that checks if the given role matches one
+// of the expected roles.
+func IsRoleIn(role auth.Role, in ...auth.Role) valerra.Condition {
+	return func() bool {
+		for _, r := range in {
+			if role == r {
+				return true
+			}
+		}
+		return false
 	}
 }
