6a11c0ecfbb4d214a102daf4262659b48617442f
The ZZ <ztimes2@pm.me>
2021-10-20 10:48:12 +0000

    app/api: add 'internal/auth/psql' package
    - Add PostgreSQL implementation of a user store

---
+++ app/api/internal/auth/psql/psql.go
@@ -0,0 +1,117 @@
+package psql
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/ztimes2/tolqin/app/api/internal/auth"
+	"github.com/ztimes2/tolqin/app/api/internal/pkg/psqlutil"
+
+	sq "github.com/Masterminds/squirrel"
+)
+
+const (
+	roleNameAdmin = "admin"
+)
+
+func roleName(r auth.Role) string {
+	switch r {
+	case auth.RoleAdmin:
+		return roleNameAdmin
+	default:
+		return ""
+	}
+}
+
+func role(s string) auth.Role {
+	switch s {
+	case roleNameAdmin:
+		return auth.RoleAdmin
+	default:
+		return auth.RoleUndefined
+	}
+}
+
+type UserStore struct {
+	db      *sqlx.DB
+	builder sq.StatementBuilderType
+}
+
+func NewUserStore(db *sqlx.DB) *UserStore {
+	return &UserStore{
+		db:      db,
+		builder: psqlutil.NewQueryBuilder(),
+	}
+}
+
+func (us *UserStore) UserByEmail(email string) (auth.User, error) {
+	query, args, err := us.builder.
+		Select("id", "email", "role", "password_hash", "password_salt", "created_at").
+		From("users").
+		Where(sq.Eq{"email": email}).
+		Limit(1).
+		ToSql()
+	if err != nil {
+		return auth.User{}, fmt.Errorf("failed to build query: %w", err)
+	}
+
+	var u user
+	if err := us.db.QueryRowx(query, args...).StructScan(&u); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return auth.User{}, auth.ErrUserNotFound
+		}
+		return auth.User{}, fmt.Errorf("faile to execute query: %w", err)
+	}
+
+	return auth.User{
+		ID:           u.ID,
+		Email:        u.Email,
+		Role:         role(u.Role),
+		PasswordHash: u.PasswordHash,
+		PasswordSalt: u.PasswordSalt,
+		CreatedAt:    u.CreatedAt,
+	}, nil
+}
+
+func (us *UserStore) CreateUser(e auth.UserCreationEntry) (auth.User, error) {
+	query, args, err := us.builder.
+		Insert("users").
+		Columns("email", "role", "password_hash", "password_salt").
+		Values(
+			e.Email,
+			roleName(e.Role),
+			e.PasswordHash,
+			e.PasswordSalt,
+		).
+		Suffix("RETURNING id, email, role, password_hash, password_salt, created_at").
+		ToSql()
+	if err != nil {
+		return auth.User{}, fmt.Errorf("failed to build query: %w", err)
+	}
+
+	var u user
+	if err := us.db.QueryRowx(query, args...).StructScan(&u); err != nil {
+		return auth.User{}, fmt.Errorf("failed to execute query: %w", err)
+	}
+
+	return auth.User{
+		ID:           u.ID,
+		Email:        u.Email,
+		Role:         role(u.Role),
+		PasswordHash: u.PasswordHash,
+		PasswordSalt: u.PasswordSalt,
+		CreatedAt:    u.CreatedAt,
+	}, nil
+}
+
+type user struct {
+	ID           string    `db:"id"`
+	Email        string    `db:"email"`
+	Role         string    `db:"role"`
+	PasswordHash string    `db:"password_hash"`
+	PasswordSalt string    `db:"password_salt"`
+	CreatedAt    time.Time `db:"created_at"`
+}
